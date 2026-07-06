b3bdf7339b3190e2408c7ed89a846cb6021cc9bf
Luiz Madaloz <8luiz1@gmail.com>
2026-07-06 12:02:19 -0300

    primeiros passos javascript

---
+++ Seção 14 Introduction to javascript ES6/class85.js
@@ -0,0 +1,5 @@
+var nome = prompt("Digite seu nome");
+nome1 = nome.slice(0,1).toUpperCase();
+nome2 = nome.slice(1,).toLowerCase();
+
+alert("Hello " + nome1 + nome2);
---
+++ Seção 14 Introduction to javascript ES6/class87.js
@@ -0,0 +1,3 @@
+var age = prompt('Qual a idade do seu cão');
+var dogage = (age -2) * 4 + 21 ;
+alert("Seu cão tem "+ dogage + " anos humanos");
