3232cdc6889fd97fa66ed16eec5a0a0f7dd1b5f1
Luiz Madaloz <8luiz1@gmail.com>
2026-07-08 06:06:09 -0300

    começando

---
+++ Seção 15 Intermediate Javascript/99. Random Number Generation in Javascript Building a Love Calculator/script.js
@@ -0,0 +1,6 @@
+var firstName = prompt("Digite seu nome");
+var secondName  = prompt("Digite o nome do seu parceiro");
+var calculo = Math.random();
+calculo = calculo *100;
+var total = Math.floor(calculo);
+alert("A chance de sucesso do relacionamento de "+ firstName+" e " + secondName +" é de " +total+"%")
