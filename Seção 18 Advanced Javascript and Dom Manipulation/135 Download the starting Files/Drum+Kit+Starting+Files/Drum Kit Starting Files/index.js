7a2d6a1d15dc704ccd197a10c7660585ac2ecf6d
Luiz Madaloz <8luiz1@gmail.com>
2026-07-16 06:18:03 -0300

    commit

---
+++ Seção 18 Advanced Javascript and Dom Manipulation/135 Download the starting Files/Drum+Kit+Starting+Files/Drum Kit Starting Files/index.js
@@ -0,0 +1,5 @@
+var qtdBotoes = document.querySelectorAll(".drum").length;
+for (var i = 0; i < qtdBotoes; i++){
+document.querySelectorAll("button")[i].addEventListener("click", function (){alert("hello")});
+};
+
