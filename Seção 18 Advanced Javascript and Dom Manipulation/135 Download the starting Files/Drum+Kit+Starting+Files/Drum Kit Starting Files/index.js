bf2e34d925a8f0b68cd8102332acad5bd13274d2
Luiz Madaloz <8luiz1@gmail.com>
2026-07-16 11:50:31 -0300

    Som está funcionando

--- Seção 18 Advanced Javascript and Dom Manipulation/135 Download the starting Files/Drum+Kit+Starting+Files/Drum Kit Starting Files/index.js
+++ Seção 18 Advanced Javascript and Dom Manipulation/135 Download the starting Files/Drum+Kit+Starting+Files/Drum Kit Starting Files/index.js
@@ -1,5 +1,12 @@
 var qtdBotoes = document.querySelectorAll(".drum").length;
-for (var i = 0; i < qtdBotoes; i++){
-document.querySelectorAll("button")[i].addEventListener("click", function (){alert("hello")});
-};
+var listaSons =["crash","kick-bass","snare","tom-1","tom-2","tom-3","tom-4"   
+]
+for (let i = 0; i < qtdBotoes; i++){
+        document.querySelectorAll(".drum")[i].addEventListener("click", function(){
+            var audio = new Audio('./sounds/' + listaSons[i] + '.mp3');
+            audio.play();
+
+        });
+
+    };
 
