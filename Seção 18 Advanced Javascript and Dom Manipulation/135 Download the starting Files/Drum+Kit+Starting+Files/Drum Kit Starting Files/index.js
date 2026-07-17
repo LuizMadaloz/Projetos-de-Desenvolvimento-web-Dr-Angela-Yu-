b97764e5da57af363a953a1626b009fcdba75fb7
Luiz Madaloz <8luiz1@gmail.com>
2026-07-17 14:38:57 -0300

    huu

--- Seção 18 Advanced Javascript and Dom Manipulation/135 Download the starting Files/Drum+Kit+Starting+Files/Drum Kit Starting Files/index.js
+++ Seção 18 Advanced Javascript and Dom Manipulation/135 Download the starting Files/Drum+Kit+Starting+Files/Drum Kit Starting Files/index.js
@@ -4,7 +4,7 @@ var listaSons =["crash","kick-bass","snare","tom-1","tom-2","tom-3","tom-4"
 for (let i = 0; i < qtdBotoes; i++){
         document.querySelectorAll(".drum")[i].addEventListener("click", function(){
             var audio = new Audio('./sounds/' + listaSons[i] + '.mp3');
-            audio.play();
+            audio.play();.
 
         });
 
