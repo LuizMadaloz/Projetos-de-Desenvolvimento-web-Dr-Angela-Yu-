4cccb640e749b1dbad00cd628dedcdaf63039f2b
Luiz Madaloz <8luiz1@gmail.com>
2026-07-19 04:56:31 -0300

    tentando adicionar as teclas

--- Seção 18 Advanced Javascript and Dom Manipulation/135 Download the starting Files/Drum+Kit+Starting+Files/Drum Kit Starting Files/index.js
+++ Seção 18 Advanced Javascript and Dom Manipulation/135 Download the starting Files/Drum+Kit+Starting+Files/Drum Kit Starting Files/index.js
@@ -1,10 +1,20 @@
 var qtdBotoes = document.querySelectorAll(".drum").length;
-var listaSons =["crash","kick-bass","snare","tom-1","tom-2","tom-3","tom-4"   
-]
+var listaSons =["crash","kick-bass","snare","tom-1","tom-2","tom-3","tom-4"  ];
+var listaTeclas =["w","a","s","d","j","k","l"];
 for (let i = 0; i < qtdBotoes; i++){
-        document.querySelectorAll(".drum")[i].addEventListener("click", function(){
-            var audio = new Audio('./sounds/' + listaSons[i] + '.mp3');
+        document.querySelectorAll(".drum")[i].addEventListener("keydown", function(){
+            var teclaApertada = event.key.toLowerCase();
+            if (teclaApertada === listaTeclas[i]){
+            switch (teclaApertada) {
+                case listaTeclas[i]:
+                    var audio = new Audio('./sounds/' + listaSons[i] + '.mp3');
             audio.play();
+                    break;
+            
+                default:
+                    break;
+            }}
+            
             var cororiginal = this.style.color;
             this.style.color="white";
             setTimeout(()=>{this.style.color =cororiginal;},500);
