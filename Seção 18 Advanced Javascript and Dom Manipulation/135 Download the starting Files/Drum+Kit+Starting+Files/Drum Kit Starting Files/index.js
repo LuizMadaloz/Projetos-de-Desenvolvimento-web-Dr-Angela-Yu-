6b32877796dc77808e586117dc7a5e594f48362a
Luiz Madaloz <8luiz1@gmail.com>
2026-07-19 06:09:57 -0300

    as teclas coresponde no teclado

--- Seção 18 Advanced Javascript and Dom Manipulation/135 Download the starting Files/Drum+Kit+Starting+Files/Drum Kit Starting Files/index.js
+++ Seção 18 Advanced Javascript and Dom Manipulation/135 Download the starting Files/Drum+Kit+Starting+Files/Drum Kit Starting Files/index.js
@@ -1,26 +1,29 @@
 var qtdBotoes = document.querySelectorAll(".drum").length;
 var listaSons =["crash","kick-bass","snare","tom-1","tom-2","tom-3","tom-4"  ];
 var listaTeclas =["w","a","s","d","j","k","l"];
-for (let i = 0; i < qtdBotoes; i++){
-        document.querySelectorAll(".drum")[i].addEventListener("keydown", function(){
-            var teclaApertada = event.key.toLowerCase();
+
+
+document.addEventListener("keydown", function(event){
+     var teclaApertada = event.key.toLowerCase();
+     for (let i = 0; i < qtdBotoes; i++){
+    
             if (teclaApertada === listaTeclas[i]){
-            switch (teclaApertada) {
-                case listaTeclas[i]:
-                    var audio = new Audio('./sounds/' + listaSons[i] + '.mp3');
+                var audio = new Audio('./sounds/' + listaSons[i] + '.mp3');
             audio.play();
-                    break;
+             
             
-                default:
-                    break;
-            }}
-            
-            var cororiginal = this.style.color;
-            this.style.color="white";
-            setTimeout(()=>{this.style.color =cororiginal;},500);
+            var botao = document.querySelectorAll(".drum")[i]; 
+            var corOriginal = botao.style.color;
+            botao.style.color="white";
+            setTimeout(()=>{botao.style.color = corOriginal;},200);
+            break
+        }
 
             
-        });
+        };
 
-    };
+});
+   
+
+    
 
