5154596d607fc845614a53bdb1da7eef1b42688e
Luiz Madaloz <8luiz1@gmail.com>
2026-07-23 06:45:02 -0300

    primeiras modificações

--- Seção 20 Boss level Challenge 2- The simon Game/Simon+Game+Challenge+Starting+Files/Simon Game Challenge Starting Files/script.js
+++ Seção 20 Boss level Challenge 2- The simon Game/Simon+Game+Challenge+Starting+Files/Simon Game Challenge Starting Files/script.js
@@ -1,4 +1,5 @@
 let gamePattern =[];
+let 
 
 function nextSequence(){
     let numberRandom = Math.floor(Math.random() * 4);
@@ -11,3 +12,31 @@ let randomChosenColour = buttonColours[nextSequence()];
 
 gamePattern.push(randomChosenColour);
 
+$("#green").on("click", function(){
+     let audio = new Audio("./sounds/green.mp3");
+     audio.play();
+     $(this).fadeTo('fast', 0.33).fadeTo('fast', 1);
+});
+
+$("#red").on("click", function(){
+    let audio = new Audio("./sounds/red.mp3");
+    audio.play();
+    $(this).fadeTo('fast', 0.33).fadeTo('fast', 1);
+});
+
+$("#yellow").on("click", function(){
+    let audio = new Audio("./sounds/yellow.mp3");
+    audio.play();
+    $(this).fadeTo('fast', 0.33).fadeTo('fast', 1);
+});
+
+$("#blue").on("click", function(){
+    let audio = new Audio("./sounds/blue.mp3");
+    audio.play();
+    $(this).fadeTo('fast', 0.33).fadeTo('fast', 1);
+});
+
+ $(".bnt").on('click',function(){
+    let userChosenColour = $(this).attr("id")
+
+})
