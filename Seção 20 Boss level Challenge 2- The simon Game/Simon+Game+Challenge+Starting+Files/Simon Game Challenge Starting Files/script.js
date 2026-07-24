7e52f05a55eb2a825b97e09c0cd78b4d94ef3d61
Luiz Madaloz <8luiz1@gmail.com>
2026-07-24 06:14:06 -0300

    avançando no projeto

--- Seção 20 Boss level Challenge 2- The simon Game/Simon+Game+Challenge+Starting+Files/Simon Game Challenge Starting Files/script.js
+++ Seção 20 Boss level Challenge 2- The simon Game/Simon+Game+Challenge+Starting+Files/Simon Game Challenge Starting Files/script.js
@@ -1,9 +1,11 @@
 let gamePattern =[];
-let 
+let userClickedPattern = [];
 
 function nextSequence(){
+    const colores =['green', 'red', 'yellow', 'bluw'];
     let numberRandom = Math.floor(Math.random() * 4);
-    return numberRandom;
+    gamePattern.push(colores[numberRandom]);
+    //return playSound(colores[numberRandom]);
 };
 
 const buttonColours =["green", "red", "yellow", "blue" ];
@@ -12,31 +14,39 @@ let randomChosenColour = buttonColours[nextSequence()];
 
 gamePattern.push(randomChosenColour);
 
-$("#green").on("click", function(){
-     let audio = new Audio("./sounds/green.mp3");
+function playSound(name){
+    if (name === 'green'){
+        let audio = new Audio("./sounds/green.mp3");
      audio.play();
-     $(this).fadeTo('fast', 0.33).fadeTo('fast', 1);
-});
-
-$("#red").on("click", function(){
-    let audio = new Audio("./sounds/red.mp3");
+     
+  
+    }else if(name === 'red'){
+               let audio = new Audio("./sounds/red.mp3");
     audio.play();
-    $(this).fadeTo('fast', 0.33).fadeTo('fast', 1);
-});
+   
 
-$("#yellow").on("click", function(){
-    let audio = new Audio("./sounds/yellow.mp3");
+    }else if(name === 'yellow'){
+        let audio = new Audio("./sounds/yellow.mp3");
     audio.play();
-    $(this).fadeTo('fast', 0.33).fadeTo('fast', 1);
-});
+    
 
-$("#blue").on("click", function(){
-    let audio = new Audio("./sounds/blue.mp3");
+    }else if (name === 'blue'){
+        let audio = new Audio("./sounds/blue.mp3");
     audio.play();
-    $(this).fadeTo('fast', 0.33).fadeTo('fast', 1);
-});
+    
+    }
+    $('#' + name).fadeTo(100, 0.33).fadeTo(100, 1);
+    $('#' + name).addClass('pressed')
+    let tempo = setTimeout(() => {
+        $('#' + name).removeClass('pressed');
+    }, 100);
+    
+};
 
- $(".bnt").on('click',function(){
-    let userChosenColour = $(this).attr("id")
-
+$('.btn').click(function(event){
+     let userChosenColour = $(this).attr("id");
+    userClickedPattern.push(userChosenColour);
+     let idClick = this.id;
+    return playSound(idClick);
 })
+
