8e728d4eb6d9d256edf15a41a4de97de48fdd39e
Luiz Madaloz <8luiz1@gmail.com>
2026-08-16 05:41:11 -0300

    get

--- Seção 24 Express.js with Node js/203. HTTP Requests/index.js
+++ Seção 24 Express.js with Node js/203. HTTP Requests/index.js
@@ -3,7 +3,15 @@ const app = express();
 const port = 3200;
 
 app.get("/", (req, res)=>{
-    res.send("Hellow");
+    res.send("<h1>Hellow</h1>");
+});
+
+app.get("/about", (req, res) =>{
+    res.send('<h1>Sobre nós</h1>');
+});
+
+app.get("/contact", (req, res) =>{
+    res.send('<h1>Nosso contato</h1>');
 });
 
 app.listen(port,()=>{
