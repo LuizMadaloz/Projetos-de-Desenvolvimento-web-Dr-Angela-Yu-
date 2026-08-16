f09f8cedb4b7743e60e943a45cfa577bd01af352
Luiz Madaloz <8luiz1@gmail.com>
2026-08-16 12:25:30 -0300

    relembrando

--- Projetos Pessoais/site para fazer revisão/script.js
+++ Projetos Pessoais/site para fazer revisão/script.js
@@ -1,4 +1,5 @@
 const h1 = document.querySelector("h1");
+const a = document.querySelector("a");
 
 h1.addEventListener("mouseover", function(){
     h1.classList.add('colorH1');
@@ -7,5 +8,15 @@ h1.addEventListener("mouseover", function(){
 h1.addEventListener("mouseout", function(){
       setTimeout(function(){
         h1.classList.remove('colorH1')
+    },100);
+});
+
+a.addEventListener("mouseover", function(){
+    a.classList.add('colorH1');
+  
+});
+a.addEventListener("mouseout", function(){
+      setTimeout(function(){
+        a.classList.remove('colorH1')
     },100);
 });
