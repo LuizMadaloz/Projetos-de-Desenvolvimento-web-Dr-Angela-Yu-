181bd3fa1c398c1eb461bd9c7bc626a1ad1d00ff
Luiz Madaloz <8luiz1@gmail.com>
2026-09-08 06:15:24 -0300

    morgan and logger middleware

--- Seção 24 Express.js with Node js/205 introduction to Middlewares/index3.js
+++ Seção 24 Express.js with Node js/205 introduction to Middlewares/index3.js
@@ -3,7 +3,14 @@ import express from "express";
 const app = express();
 const port = 3000;
 
-app.use(logger);
+function logger(req,res,next) {
+  console.log("request method: ", req.method);
+  console.log('request url: ', req.url);
+next();
+};
+
+
+app.use(logger)
 
 app.get("/", (req, res) => {
   res.send("Hello");
